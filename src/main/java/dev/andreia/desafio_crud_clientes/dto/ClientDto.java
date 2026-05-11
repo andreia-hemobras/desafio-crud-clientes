356ccac99c59093b24f01e2d8e54c05ec603291a
andreia-hemobras <andreia.hemobras@gmail.com>
2026-05-11 11:54:44 -0300

    ClientDto

---
+++ src/main/java/dev/andreia/desafio_crud_clientes/dto/ClientDto.java
@@ -0,0 +1,60 @@
+package dev.andreia.desafio_crud_clientes.dto;
+
+import dev.andreia.desafio_crud_clientes.entities.Client;
+
+import java.time.LocalDate;
+
+public class ClientDto {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDto() {
+    }
+
+    public ClientDto(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDto(Client client){
+        id = client.getId();
+        name = client.getName();
+        cpf = client.getCpf();
+        income = client.getIncome();
+        birthDate = client.getBirthDate();
+        children = client.getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
